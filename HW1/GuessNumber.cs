54c5b450b33b75ed0eda442209908958c290485f
Chiaan <45359891+dan870627@users.noreply.github.com>
2025-04-28 03:07:31 -0700

    Update GuessNumber.cs
    update HW1
    add game over result

--- HW1/GuessNumber.cs
+++ HW1/GuessNumber.cs
@@ -9,6 +9,7 @@ namespace GuessNumber
             int x = 0;
             int y = 99;
             int ans = rng.Next(0, 99);
+            Console.WriteLine("ans = {0}", ans);
             bool check = true;
 
             while (check)
@@ -16,9 +17,19 @@ namespace GuessNumber
                 Console.WriteLine("({0}, {1})?", x, y);
 
                 int guess = int.Parse(Console.ReadLine());
+                if (x == ans && guess == ans + 1)
+                {
+                    Console.WriteLine("You loss!");
+                    break;
+                }
+                if (y == ans && guess == ans - 1)
+                {
+                    Console.WriteLine("You loss!");
+                    break;
+                }
                 if (guess <= x || guess >= y)
                 {
-                    Console.WriteLine("超出範圍, tryagain.");
+                    Console.WriteLine("out of rang, tryagain.");
                 }
                 if (guess == ans)
                 {
@@ -27,8 +38,8 @@ namespace GuessNumber
                 }
                 else
                 {
-                    if (guess > x && ans > guess) x = guess;
-                    if (guess < y && ans < guess) y = guess;
+                    if (guess > x && ans > guess) x = guess + 1;
+                    if (guess < y && ans < guess) y = guess - 1;
                 }
             }
         }
