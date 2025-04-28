e49280c59d7e168e1aad9768013753a71a5e5e1b
Chiaan <45359891+dan870627@users.noreply.github.com>
2025-04-28 03:59:44 -0700

    Update GuessNumber.cs
    Add two computer guess(random / dichotomy)
    Add Guess result

--- HW1/GuessNumber.cs
+++ HW1/GuessNumber.cs
@@ -4,30 +4,33 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("猜數字");
+            Console.WriteLine("Guess Number");
             Random rng = new Random();
             int x = 0;
             int y = 99;
             int ans = rng.Next(0, 99);
-            Console.WriteLine("ans = {0}", ans);
+            // Console.WriteLine("ans = {0}", ans);
             bool check = true;
 
+            //Guess result
+            int people_guess_count = 0;
+            int computer_guess_count_RandomInRange = 0;
+            int computer_guess_count_dichotomy = 0;
+
+            // People Guess
             while (check)
             {
                 Console.WriteLine("({0}, {1})?", x, y);
 
                 int guess = int.Parse(Console.ReadLine());
-                if (x == ans && guess == ans + 1)
+                people_guess_count++;
+                if ((x == ans && guess == ans + 1) || (y == ans && guess == ans - 1))
                 {
                     Console.WriteLine("You loss!");
+                    people_guess_count = 99;
                     break;
                 }
-                if (y == ans && guess == ans - 1)
-                {
-                    Console.WriteLine("You loss!");
-                    break;
-                }
-                if (guess <= x || guess >= y)
+                if (guess < x || guess > y)
                 {
                     Console.WriteLine("out of rang, tryagain.");
                 }
@@ -40,6 +43,86 @@ namespace GuessNumber
                 {
                     if (guess > x && ans > guess) x = guess + 1;
                     if (guess < y && ans < guess) y = guess - 1;
+                }
+            }
+
+            //Computer Guess(Random in rang)
+            x = 0;
+            y = 99;
+            check = true;
+            while (check)
+            {
+                //Console.WriteLine("({0}, {1})?", x, y);
+
+                int guess = rng.Next(x, y);
+                computer_guess_count_RandomInRange++;
+                if ((x == ans && guess == ans + 1) || (y == ans && guess == ans - 1))
+                {
+                    computer_guess_count_RandomInRange = 99;
+                    break;
+                }
+                if (guess == ans)
+                {
+                    check = false;
+                }
+                else
+                {
+                    if (guess > x && ans > guess) x = guess + 1;
+                    if (guess < y && ans < guess) y = guess - 1;
+                }
+            }
+
+            //Computer Guess(dichotomy)
+            x = 0;
+            y = 99;
+            check = true;
+            while (check)
+            {
+                //Console.WriteLine("({0}, {1})?", x, y);
+
+                int guess = ((y - x) / 2) + x;
+                computer_guess_count_dichotomy++;
+                if ((x == ans && guess == ans + 1) || (y == ans && guess == ans - 1))
+                {
+                    computer_guess_count_dichotomy = 99;
+                    break;
+                }
+                if (guess == ans)
+                {
+                    check = false;
+                }
+                else
+                {
+                    if (guess > x && ans > guess) x = guess + 1;
+                    if (guess < y && ans < guess) y = guess - 1;
+                }
+            }
+            Console.WriteLine("~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Guess Number result:");
+            Console.WriteLine("User: {0}", people_guess_count);
+            Console.WriteLine("Computer(RandomInRange): {0}", computer_guess_count_RandomInRange);
+            Console.WriteLine("Computer(Dichotomy): {0}", computer_guess_count_dichotomy);
+
+            if (people_guess_count < computer_guess_count_RandomInRange)
+            {
+                if (people_guess_count < computer_guess_count_dichotomy)
+                {
+                    Console.WriteLine("People Win!");
+                }
+                else
+                {
+                    Console.WriteLine("Computer(Dichotomy) Win!");
+                }
+            }
+            else
+            {
+                if (computer_guess_count_RandomInRange < computer_guess_count_dichotomy)
+                {
+                    Console.WriteLine("Computer(RandomInRange) Win!");
+                }
+                else
+                {
+                    Console.WriteLine("Computer(Dichotomy) Win!");
                 }
             }
         }
